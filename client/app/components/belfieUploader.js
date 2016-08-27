5856cd8cdfefd9fa4b5c2421cc560a55ba6f9d99
Jack Ford <jford@salsify.com>
2016-08-27 19:28:32 -0400

    Consistency

--- client/app/components/belfieUploader.js
+++ client/app/components/belfieUploader.js
@@ -12,7 +12,7 @@ import {
 
 import { RNUploader } from 'NativeModules';
 
-export default class belfieUploader extends React.Component {
+export default class belfieUploader extends Component {
   constructor(props) {
     super(props);
 
