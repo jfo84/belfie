76c0ffbda9d5738eeaa229ff0aaf10e55a1a7de2
Jack Ford <jford@salsify.com>
2016-08-28 15:15:56 -0400

    Back to back end. Formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,18 @@ class User < ActiveRecord::Base
   include RailsMultitenant::MultitenantModel
 
   multitenant_on :account_id
-  has_many :friendships, foreign_key: :sender_user_id, dependent: :destroy
-  has_many :reverse_friendships, class_name: :Friendship, foreign_key: :receiver_user_id, dependent: :destroy
-  has_many :friends, class_name: :User, through: :friendships, source: :receiver_user
+
+  has_many :friendships,
+    foreign_key: :sender_user_id,
+    dependent: :destroy
+  has_many :reverse_friendships,
+    class_name: :Friendship,
+    foreign_key: :receiver_user_id,
+    dependent: :destroy
+  has_many :friends,
+    class_name: :User,
+    through: :friendships,
+    source: :receiver_user
   has_many :images
   has_many :friendship_requests
 
